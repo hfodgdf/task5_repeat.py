5d7e51cf65bee8053ac71dc0111e0e5c1868e9cf
hfodgdf <kakkaaosos@gmail.com>
2025-10-31 21:02:25 +0300

    Update task4_repeat.py

--- task4_repeat.py
+++ task4_repeat.py
@@ -1,10 +1,5 @@
 import math
 
-# Request radius from the user
-radius = float(input("Enter the radius of the circle: "))
-
-# Calculate the area of the circle
+radius = float(input("Введіть радіус кола: "))
 area = math.pi * radius ** 2
-
-# Display the result
-print("The area of the circle is:", area)
+print("Площа кола дорівнює:", area)
